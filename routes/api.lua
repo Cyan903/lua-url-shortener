d9d0305aa2868ad2eb6a9f6062d918aec8e3b113
CyanPiano <cyanp0515@gmail.com>
2021-10-17 04:30:38 -0400

    Lightweight sql wrapper

--- routes/api.lua
+++ routes/api.lua
@@ -1,23 +1,9 @@
-local driver = require "luasql.mysql"
-local config = require "utils.config"
-
-local env = assert(driver.mysql())
-local con = assert(env:connect(
-    config.sql.db, config.sql.user, config.sql.password
-))
-
+local sql = require "utils.sql"
 
 return {
     all = function()
-        local cur = assert(con:execute("SELECT  * FROM urls;"))
-        local row = cur:fetch({}, "a")
-        local result = ""
+        local urls = sql.fetchAll("SELECT * FROM urls;")
         
-        while row do        
-            result = result..string.format("id: %s url: %s short: %s", row.id, row.url, row.short)
-            row = cur:fetch(row, "a")
-        end
-
-        return result
+        return urls
     end
 }
---
+++ utils/sql.lua
@@ -0,0 +1,29 @@
+local driver = require "luasql.mysql"
+local config = require "utils.config"
+local JSON = require "JSON"
+local sql = {}
+
+local env = assert(driver.mysql())
+local con = assert(env:connect(
+    config.sql.db, config.sql.user, config.sql.password
+))
+
+sql.fetchOne = function()
+
+end
+
+sql.fetchAll = function(query)
+    local cur = assert(con:execute(query))
+    local row = cur:fetch({}, "a")
+    local result = ""
+    
+    while row do
+        result = JSON:encode(row)..","..result
+        row = cur:fetch(row, "a")
+    end
+
+    cur:close()
+    return string.format("{\"data\": [%s]}", result:sub(1, -2))
+end
+
+return sql
